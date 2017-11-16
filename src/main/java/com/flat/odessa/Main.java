d4a0711afc2a2d970b73ebb3e097ec606babacea
GreySlon <garanenkosergii@gmail.com>
2017-11-17 01:54:51 +0200

    one page iterable

--- src/main/java/com/flat/odessa/Main.java
+++ src/main/java/com/flat/odessa/Main.java
@@ -1,15 +1,17 @@
 package com.flat.odessa;
 
 import com.flat.odessa.entity.Links;
+import com.flat.odessa.entity.SinglePageCrawler;
+
+import java.net.URL;
 
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) {
     String url = args[0];
 
-    Links links = new Links(url);
-
-    for (String link : links) {
+    Links links = new SinglePageCrawler(url);
+    for (URL link : links) {
       System.out.println(link);
     }
   }
--- src/test/java/com/flat/odessa/entity/LinkTest.java
+++ src/test/java/com/flat/odessa/entity/LinkTest.java
@@ -1,11 +1,12 @@
 package com.flat.odessa.entity;
 
+import java.net.URL;
+
 public class LinkTest {
 
   public static void main(String[] args) {
-    Links links = new Links("https://github.com");
-
-    for (String link : links) {
+    Links links = new SinglePageCrawler("https://github.com");
+    for (URL link : links) {
       System.out.println(link);
     }
   }
